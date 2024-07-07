b6859e8c01b8720c5d73e5ae76e6a59804126981
Saurabh Zinjad <zinjadsaurabh1997@gmail.com>
2024-07-06 19:09:03 -0700

    Setup and Install  NextJs as Web Client

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { PortfolioPage } from "@/components/portfolio-page";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <PortfolioPage/>
+  );
+}
