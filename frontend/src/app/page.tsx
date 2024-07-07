b9c687b002be3870c4d799663c0ec1634cd81898
Saurabh Zinjad <zinjadsaurabh1997@gmail.com>
2024-07-07 01:11:29 -0700

    ENH: Add Carousel Functionality

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,9 @@
-import { PortfolioPage } from "@/components/portfolio-page";
-import Image from "next/image";
+import Portfolio from "@/components/portfolio/portfolio";
 
 export default function Home() {
   return (
-    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <PortfolioPage/>
+    <>
+    <Portfolio/>
+    </>
   );
 }
